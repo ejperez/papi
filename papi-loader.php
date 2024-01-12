00c9277b7db39e0e1d972f8bb29d28c6d8b1c0e5
EJ Perez <ej.perez@stok.se>
2024-01-12 21:08:28 +0800

    Update version

--- papi-loader.php
+++ papi-loader.php
@@ -2,10 +2,10 @@
 
 /**
  * Plugin Name: Papi
- * Description: Page Type API with custom fields
- * Author: Fredrik Forsmo
+ * Description: Page Type API with custom fields (forked)
+ * Author: EJ Perez
  * Author URI: https://frozzare.com
- * Version: 3.3.0-dev
+ * Version: 3.4.0-dev
  * Plugin URI: https://wp-papi.github.io
  * Textdomain: papi
  * Domain Path: /languages/
